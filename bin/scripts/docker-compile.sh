9c290989b9d6197c3e14b8742810e6fa0e72d552
tihoangyeudau <tuantrung2905@gmail.com>
2020-07-05 03:05:11 +0700

    change link restart webui + fix link docker installer

--- bin/scripts/docker-compile.sh
+++ bin/scripts/docker-compile.sh
@@ -2,7 +2,7 @@
 
 DOCKER="docker"
 # Git repo url of Hyperion
-GIT_REPO_URL="https://github.com/hyperion-project/hyperion.ng.git"
+GIT_REPO_URL="https://github.com/tihoangyeudau/hyperion.ng-1.git"
 # cmake build type
 BUILD_TYPE="Release"
 # the image tag at hyperionproject/hyperion-ci
