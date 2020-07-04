e4d52efaefc542c998c169f2c4ce2d54df116bd3
tihoangyeudau <tuantrung2905@gmail.com>
2020-07-05 02:17:00 +0700

    update some change link and fix vietnamese

--- assets/webconfig/js/content_dashboard.js
+++ assets/webconfig/js/content_dashboard.js
@@ -30,7 +30,7 @@ $(document).ready( function() {
 // 	{
 // 		var h = '<span style="color:red;font-weight:bold">'+$.i18n('dashboard_newsbox_noconn')+'</span>';
 // 		$.ajax({
-// 			url: 'https://hyperion-project.org/wp-json/wp/v2/posts?_embed',
+// 			url: 'https://www.facebook.com/Smart.led.strip.light',
 // 			dataType: 'json',
 // 			type: 'GET',
 // 			timeout: 2000
